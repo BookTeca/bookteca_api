48a7832f0086c0cdd203d5c593c5be234da1b667
Marcela <mamadipaula@gmail.com>
2023-03-07 11:14:53 -0300

    feat: CopyModel

--- copies/models.py
+++ copies/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 
-# Create your models here.
 
+class StateOptions(models.TextChoices):
+    NEW = "Novo"
+    USED = "Usado"
+    DAMAGE = "Danificado"
+
+
+class Copy(models.Model):
+    class Meta:
+        ordering = ["id"]
+
+    is_available = models.BooleanField(default=True)
+    state = models.CharField(max_length=10, choices=StateOptions.choices, default=StateOptions.NEW)
+
+    book = models.ForeignKey(
+        "books.Book",
+        on_delete=models.CASCADE,
+        related_name="book_copies",
+    )
