9a4d403c22fc8e0045c0467e3d23861f2f6bf71b
bj-mandim <beatrizjuliomandim@gmail.com>
2023-03-08 19:08:34 -0300

    Borrowing: updated view

--- _bookteca/urls.py
+++ _bookteca/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("books.urls")),
     path("api/", include("users.urls")),
+    path("api/", include("copies.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
 ]
--- copies/urls.py
+++ copies/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from copies.views import CopyView, CopieDetailView
+from copies.views import CopyView, CopyDetailView, BorrowingsView, BorrowingsDetailView
 
 
 urlpatterns = [
     path("books/<int:book_id>/copies/", CopyView.as_view()),
-    path("copies/<int:copy_id>/", CopieDetailView.as_view())
+    path("copies/<int:copy_id>/", CopyDetailView.as_view()),
+    path("borrowings/", BorrowingsView.as_view()),
+    path("borrowings/<int:borrowing_id>/", BorrowingsDetailView.as_view())
+
 ]
-# borrowings/
-# borrowings/<int:borrowing_id>/
