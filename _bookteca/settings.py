62d52a6f4fc337f0dd18748079e820cc8fa90643
Petrus Lobato <admpetrusmlb@gmail.com>
2023-03-13 19:23:40 -0300

    initialization deploy

--- _bookteca/settings.py
+++ _bookteca/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import dotenv
-
+from django.core.management.utils import get_random_secret_key
 dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
