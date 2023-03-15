88ee44802e2f53fd62d534b165c3b716ff0b71ca
Viviane Ribeiro <vivy.saribeiro@gmail.com>
2023-03-15 15:12:32 -0300

    fix: staticfiles

--- _bookteca/settings.py
+++ _bookteca/settings.py
@@ -187,7 +187,7 @@ APSCHEDULER_RUN_NOW_TIMEOUT = 25  # Seconds
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
